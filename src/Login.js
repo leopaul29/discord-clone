c0ff2069e5fb4b504305d6e009df69b30c715047
Leo-paul <leopaulmartin@hotmail.fr>
2020-10-11 23:08:39 +0200

    login page style

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Login.css'
+import { Button } from '@material-ui/core'
 
 function Login() {
+    const signIn = () => {
+
+    }
+
     return (
         <div className="login">
-            <h2>I am the login page</h2>
+            <div className="login__logo">
+                <img src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/800px-Discord_logo.svg.png" 
+                alt=""/>
+            </div>
+
+            <Button onClick={signIn}>Sign In</Button>
         </div>
     )
 }
