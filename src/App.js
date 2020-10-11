cb6ed1022502089d08be1cb0e813e0076da101be
Leo-paul <leopaulmartin@hotmail.fr>
2020-10-11 22:55:28 +0200

    setup and display login component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar.js';
+import { selectUser } from './features/userSlice'
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Sidebar/>
-      <Chat/>
+      {user ? (
+        <>
+        <Sidebar/>
+        <Chat/>
+        </>
+      ): (
+        <Login />
+      )}
     </div>
   );
 }
