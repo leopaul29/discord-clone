eb3bbc7acfda748deb137fb0d8f683178e386f4f
Leo-paul <leopaulmartin@hotmail.fr>
2020-10-13 23:37:33 +0200

    setup Google login popup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar.js';
 import { selectUser } from './features/userSlice'
 import Login from './Login';
+import { auth } from './firebase';
+import { login, logout } from "./features/userSlice"
 
 function App() {
+  // push data to redux store
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        // the ser is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        // the user is log out
+        dispatch(logout())
+      }
+    });
+  },[dispatch]);
 
   return (
     <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { auth, provider } from './firebase'
 import './Login.css'
 import { Button } from '@material-ui/core'
 
 function Login() {
     const signIn = () => {
-
+        auth.signInWithPopup(provider).catch((error) => alert(error.message))
     }
 
     return (
